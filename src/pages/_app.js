2b7823b3b7d6a10860673e65bded124ea09a94a6
Riyan Saputra <alamat.emailnya.riyan@gmail.com>
2023-04-02 05:54:19 +0700

    build: implement redux, rtk, redux-persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
+// Stylesheet
 import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+// Redux
+import { wrapper } from '@/redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+export default wrapper.withRedux(({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+        <Component {...props.pageProps} />
+      </PersistGate>
+    </Provider>
+  );
+});
